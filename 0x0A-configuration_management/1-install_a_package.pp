26df3efbacb2f67c59cb46c8eac5c3b629c3e56c
Chinweike99 <Innocentakwolu1@gmail.com>
2024-01-19 07:45:33 -0800

    Installs flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# use Puppet to install Flask from pip3
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
-}
+# installs flask from pip3
 
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
